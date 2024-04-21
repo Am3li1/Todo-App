852bd8544c417ccb3733cbdf7f944f850abd9e13
Am3li1 <ameliasantosh2304@gmail.com>
2024-04-21 20:56:45 +0530

    modified models.g

--- server/models/models.go
+++ server/models/models.go
@@ -1,3 +1,12 @@
 import models
 
-import
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type ToDoList struct{
+	ID			primitive.ObjectID			`json:"_id,omitempty" bson:"_id,o"`
+	Task		starting					`json: "task, onitempty"`
+	Status		bool						`json:"status, onitempty"`
+}
+
+
+
